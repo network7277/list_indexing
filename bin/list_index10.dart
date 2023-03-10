726621072bc5dc4b1a6f2c96c6c9ddfc735cb651
network7277 <husanjurayev7277@gmail.com>
2023-03-10 23:41:40 +0500

    10

--- bin/list_index10.dart
+++ bin/list_index10.dart
@@ -6,4 +6,18 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func( List <int> list_num ){
+    int i = 0;
+    int a = list_num[0];
+    List list1 = [a];
+    while ( i < list_num.length ){
+        if ( list_num[i] >= a ){
+            a = list_num[i];
+        }
+        i++;
+    }
+    return list1;
+}
+void main() {
+    print(func([12, 2, 5, 2, 7, 9, 1]));
+}
